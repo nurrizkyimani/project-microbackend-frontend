380a865f007dac2c3023bdd75e212edd60ca96c8
bocahrokok <indieunistudy@gmail.com>
2020-08-05 13:57:02 +0700

    first commit

---
+++ src/components/layout.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+import Header from "./header";
+
+function Layout({ children }) {
+  return (
+    <div className="flex flex-col min-h-screen font-sans text-gray-900">
+      <Header />
+
+      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
+        {children}
+      </main>
+
+      <footer className="bg-blue-700">
+        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
+          <p className="text-white">
+            
+            
+          </p>
+        </nav>
+      </footer>
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
